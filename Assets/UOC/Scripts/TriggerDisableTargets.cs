041025469672fd16687f2e85434ae0fe843ca0a6
xDavidLeon <david@linceworks.com>
2017-04-16 19:31:19 +0200

    Trigger Disable Targets

---
+++ Assets/UOC/Scripts/TriggerDisableTargets.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerDisableTargets : MonoBehaviour {
+
+	public GameObject[] targets;
+
+	void OnTriggerEnter(Collider c)
+	{
+		// Ignorar todo lo que no sea el jugador
+		if (c.CompareTag ("Player") == false)
+			return; 
+		
+		foreach (GameObject g in targets) {
+			g.SetActive (false);
+		}
+	}
+}
