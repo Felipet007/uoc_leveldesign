c792d7b066b8211190d0496bad1e9cce88648398
David León <david@linceworks.com>
2017-04-18 23:08:11 +0200

    Potion and Enemy Spawn

---
+++ Assets/UOC/Scripts/PotionTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PotionTrigger : MonoBehaviour {
+    public int healAmount = 50;
+
+	void OnTriggerEnter(Collider c)
+    {
+        if (c.CompareTag("Player") == false) return;
+
+        PlayerHealth health = c.GetComponent<PlayerHealth>();
+
+        int hp = health.currentHealth;
+        hp += healAmount;
+        if (hp > 100) hp = 100;
+
+        health.currentHealth = hp;
+
+        GameObject.Destroy(this.gameObject);
+    }
+}
---
+++ Assets/UOC/Scripts/TriggerEnemySpawn.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerEnemySpawn : MonoBehaviour {
+    
+    [System.Serializable]
+    public struct EnemySpawn
+    {
+        public GameObject enemyPrefab;
+        public Vector3 offset;
+        public int quantity;
+    }
+
+    public List<EnemySpawn> groups;
+
+	void OnTriggerEnter(Collider c)
+    {
+        if (c.CompareTag("Player") == false) return;
+
+        foreach (EnemySpawn spawn in groups)
+        {
+            for (int i = 0; i < spawn.quantity; i++)
+            {
+                GameObject.Instantiate(spawn.enemyPrefab, transform.position + spawn.offset, Quaternion.identity);
+            }
+        }
+
+        GameObject.Destroy(this.gameObject);
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        foreach (EnemySpawn spawn in groups)
+        {
+            Gizmos.DrawSphere(transform.position + spawn.offset, 0.5f);
+        }
+        
+    }
+}
