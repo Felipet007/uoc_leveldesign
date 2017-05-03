819ac354e4917a4c7ac991e6c953713862731712
xDavidLeon <david@linceworks.com>
2017-05-04 00:19:16 +0200

    Level Loading

---
+++ Assets/UOC/Scripts/TriggerLoadLevel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerLoadLevel : MonoBehaviour {
+	public string sceneToLoad;
+
+	void OnTriggerEnter(Collider c)
+    {
+        if (c.CompareTag("Player") == false) return;
+
+		UnityEngine.SceneManagement.SceneManager.LoadScene (sceneToLoad);
+    }
+}
