4a04d50ca5866694fed3b3092fad847ffc064cf0
David León <david@linceworks.com>
2019-05-25 17:05:37 +0200

    Fixes

--- Assets/_Complete-Game/Scripts/Camera/CameraFollow.cs
+++ Assets/_Complete-Game/Scripts/Camera/CameraFollow.cs
@@ -5,19 +5,17 @@ namespace CompleteProject
 {
     public class CameraFollow : MonoBehaviour
     {
-        public Transform target;            // The position that that camera will be following.
-        public float smoothing = 5f;        // The speed with which the camera will be following.
+        public Transform target;    // The position that that camera will be following.
+        public float smoothing = 5f;    // The speed with which the camera will be following.
+        public float smoothingRotation = 5f;    // The speed with which the camera will be rotating to aim at the player.
 
-
-        public Vector3 offset;                     // The initial offset from the target.
-
+        public Vector3 offset;  // The initial offset from the target.
 
         void Start ()
         {
             // Calculate the initial offset.
-            //offset = transform.position - target.position;
+            transform.position = target.position + offset;
         }
-
 
         void FixedUpdate ()
         {
@@ -26,6 +24,11 @@ namespace CompleteProject
 
             // Smoothly interpolate between the camera's current position and it's target position.
             transform.position = Vector3.Lerp (transform.position, targetCamPos, smoothing * Time.deltaTime);
+
+            // Smoothly interpolate rotation to look at the player position
+            Vector3 targetPosition = target.position - transform.position;
+            Quaternion newRotation = Quaternion.LookRotation(targetPosition); // Create a new rotation that aims at the player position
+            transform.rotation = Quaternion.Lerp(transform.rotation, newRotation, smoothingRotation * Time.deltaTime);
         }
     }
 }
