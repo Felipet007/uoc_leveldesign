6a5941e8bf4aea0246211f338615c7705493f6ee
David León <david@linceworks.com>
2019-05-25 16:37:49 +0200

    Updated to Unity 2019.1.3f1

--- Assets/_Complete-Game/Scripts/Camera/CameraFollow.cs
+++ Assets/_Complete-Game/Scripts/Camera/CameraFollow.cs
@@ -5,17 +5,19 @@ namespace CompleteProject
 {
     public class CameraFollow : MonoBehaviour
     {
-        public Transform target;    // The position that that camera will be following.
-        public float smoothing = 5f;    // The speed with which the camera will be following.
-        public float smoothingRotation = 5f;    // The speed with which the camera will be rotating to aim at the player.
+        public Transform target;            // The position that that camera will be following.
+        public float smoothing = 5f;        // The speed with which the camera will be following.
 
-        public Vector3 offset;  // The initial offset from the target.
+
+        Vector3 offset;                     // The initial offset from the target.
+
 
         void Start ()
         {
             // Calculate the initial offset.
-            transform.position = target.position + offset;
+            offset = transform.position - target.position;
         }
+
 
         void FixedUpdate ()
         {
@@ -24,11 +26,6 @@ namespace CompleteProject
 
             // Smoothly interpolate between the camera's current position and it's target position.
             transform.position = Vector3.Lerp (transform.position, targetCamPos, smoothing * Time.deltaTime);
-
-            // Smoothly interpolate rotation to look at the player position
-            Vector3 targetPosition = target.position - transform.position;
-            Quaternion newRotation = Quaternion.LookRotation(targetPosition); // Create a new rotation that aims at the player position
-            transform.rotation = Quaternion.Lerp(transform.rotation, newRotation, smoothingRotation * Time.deltaTime);
         }
     }
 }
--- Assets/_Complete-Game/Scripts/Enemy/EnemyMovement.cs
+++ Assets/_Complete-Game/Scripts/Enemy/EnemyMovement.cs
@@ -5,13 +5,11 @@ namespace CompleteProject
 {
     public class EnemyMovement : MonoBehaviour
     {
-        public float moveSpeed = 0.5f;
-        public bool useNavMesh = false;
-
         Transform player;               // Reference to the player's position.
         PlayerHealth playerHealth;      // Reference to the player's health.
         EnemyHealth enemyHealth;        // Reference to this enemy's health.
         UnityEngine.AI.NavMeshAgent nav;               // Reference to the nav mesh agent.
+
 
         void Awake ()
         {
@@ -19,39 +17,23 @@ namespace CompleteProject
             player = GameObject.FindGameObjectWithTag ("Player").transform;
             playerHealth = player.GetComponent <PlayerHealth> ();
             enemyHealth = GetComponent <EnemyHealth> ();
-            nav = GetComponent<UnityEngine.AI.NavMeshAgent>();
+            nav = GetComponent <UnityEngine.AI.NavMeshAgent> ();
         }
+
 
         void Update ()
         {
             // If the enemy and the player have health left...
             if(enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0)
             {
-                if (useNavMesh)
-                {
-                    // ... set the destination of the nav mesh agent to the player.
-                    nav.SetDestination(player.position);
-                }
-                else
-                {
-                    // Not using navmesh, just go towards the player if he's close
-                    float distanceToPlayer = Vector3.Distance(player.position, transform.position);
-                    if (distanceToPlayer < 10.0f)
-                    {
-                        Vector3 dirToPlayer = player.position - transform.position;
-                        transform.position += dirToPlayer * Time.deltaTime * moveSpeed;
-                        transform.LookAt(player);
-                    }
-                }
+                // ... set the destination of the nav mesh agent to the player.
+                nav.SetDestination (player.position);
             }
             // Otherwise...
             else
             {
-                if (useNavMesh)
-                {
-                    // ... disable the nav mesh agent.
-                    nav.enabled = false;
-                }
+                // ... disable the nav mesh agent.
+                nav.enabled = false;
             }
         }
     }
